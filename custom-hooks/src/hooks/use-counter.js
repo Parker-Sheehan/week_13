79a3103b886314728b0f13b4f60dba0b5955cb43
Parker Sheehan <Parkersheehan007@gmail.com>
2023-04-19 12:18:43 -0500

    custom hooks

---
+++ custom-hooks/src/hooks/use-counter.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+
+const useCounter = (param) => {
+    const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + param);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [param]);
+
+  return counter
+}
+
+export default useCounter
